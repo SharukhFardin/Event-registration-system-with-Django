a7abd2e21f63d1c3e3b2721f2baae1324d612a92
Sharukh Fardin <sharukhfardin1234@gmail.com>
2024-08-14 00:25:09 +0600

    Created Event and EventRegistration Model

--- myproject/eventio/models.py
+++ myproject/eventio/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from accountio.models import User
 
-# Create your models here.
+from sharedio.models import BaseModelWithUID
+
+
+class Event(BaseModelWithUID):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    date = models.DateField()
+    time = models.TimeField()
+    location = models.CharField(max_length=200)
+    available_slots = models.PositiveIntegerField(default=10)
+
+    def __str__(self):
+        return self.title
+
+
+class EventRegistration(models.Model):
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ("event", "user")
+
+    def __str__(self):
+        return f"{self.user.username} - {self.event.title}"

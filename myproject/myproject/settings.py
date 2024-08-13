8b60000bb0c7b2be8c47708c301c042382b8155c
Sharukh Fardin <sharukhfardin1234@gmail.com>
2024-08-13 21:58:36 +0600

    Added requirements.txt and created another app for common functionalities

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -39,6 +39,9 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "accountio",
     "eventio",
+    "sharedio",
+    "rest_framework",
+    "rest_framework_simplejwt",
 ]
 
 MIDDLEWARE = [
